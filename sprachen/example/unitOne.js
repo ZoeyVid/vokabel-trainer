313eb71659ac17ac4f2c7aaa69f7210545403b42
David <48714689+DavidCraftDev@users.noreply.github.com>
2022-05-31 19:19:59 +0200

    Create unitOne.js

---
+++ sprachen/example/unitOne.js
@@ -0,0 +1,20 @@
+var name = ""
+var vocabs = [
+	{
+		"1": {
+			"german": "",
+			"vocab": "",
+			"note": ""
+		},
+		"3": {
+			"german": "",
+			"vocab": "",
+			"note": ""
+		},
+		"3": {
+			"german": "",
+			"vocab": "",
+			"note": ""
+		}
+	}
+]
