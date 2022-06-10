f8ce18c993f015ff8f81a58147bf48b06c482472
David <48714689+DavidCraftDev@users.noreply.github.com>
2022-06-10 14:41:59 +0000

    Test

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -1,6 +1,7 @@
 async function loadJSON(path) {
     try {
         var request = fetch(String(path))
+        await console.log(request)
         return (await request).json
     } catch (err) {
         console.error(String(err))
