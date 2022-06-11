fc71549df1fcbd2829ad1354d3a1b9e514978ac0
David <48714689+DavidCraftDev@users.noreply.github.com>
2022-06-11 14:50:13 +0000

    Test Navbar

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -13,7 +13,26 @@ async function loadJSON(path) {
     })
 }
 
-async function title(title) {
+async function loadHTML(path) {
+    return new Promise((resolve, reject) => {
+        var xobj = new XMLHttpRequest()
+        xobj.overrideMimeType("application/json")
+        xobj.open('GET', path, true)
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                return Resolve(xobj.responseText)
+            }
+        }
+        xobj.send(null)
+        return undefined
+    })
+}
+
+async function setTitle(title) {
     var config = await loadJSON("./config.json")
     document.title = String(title) + " - " + String(config.schulname) + " | Vokabeltrainer"
+}
+
+async function loadNavbar() {
+    document.getElementsByTagName("body")[0].innerHTML = loadHTML("./assets/html/navbar.html")
 }
