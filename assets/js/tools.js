04e89f090a9409dd3845187a42d18639b4da6abf
David <48714689+DavidCraftDev@users.noreply.github.com>
2022-06-11 14:24:03 +0000

    Mal schauen ob es der Copilot besser macht

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -1,9 +1,24 @@
-async function loadJSON(path) {
+/*async function loadJSON(path) {
     try {
         fetch(new URL(path)).then(response => {return response.json()})
     } catch (err) {
         console.error(String(err))
     }
+}*/
+
+//Get local JSON File
+async function loadJSON(path) {
+    return new Promise((resolve, reject) => {
+        var xobj = new XMLHttpRequest()
+        xobj.overrideMimeType("application/json")
+        xobj.open('GET', path, true)
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+                resolve(JSON.parse(xobj.responseText))
+            }
+        }
+        xobj.send(null)
+    })
 }
 
 async function title(title) {
