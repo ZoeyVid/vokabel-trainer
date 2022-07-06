c3225ba4a6ffe255b34555713e3ef0dd9acd777b
David <48714689+DavidCraftDev@users.noreply.github.com>
2022-07-06 13:13:06 +0000

    Bessere Farbe

--- assets/js/tools.js
+++ assets/js/tools.js
@@ -131,7 +131,7 @@ async function loadVocabs(div) {
         var vocab = vocabs[i]
         var vocabDiv = document.createElement("li")
         vocabDiv.className = "p-3"
-        vocabDiv.innerHTML = `<a class="underline font-bold">${vocab.german} - ${vocab.vocab}</a><p>${vocab.phrase} - ${vocab.note}</p><input type="checkbox" checked="checked" class="checkbox checkbox-accent" /></li>`
+        vocabDiv.innerHTML = `<a class="underline font-bold">${vocab.german} - ${vocab.vocab}</a><p>${vocab.phrase} - ${vocab.note}</p><input type="checkbox" checked="checked" class="checkbox checkbox-secondary" /></li>`
         vocabsDiv.appendChild(vocabDiv)
     }
 }
